e64b5405f74ba424a76e0d0e78c5898c02d01841
dandnirv7 <dandnirv7@gmail.com>
2023-08-26 19:42:27 +0700

    refactor: flowbite configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ 
+  content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        99: "99",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
