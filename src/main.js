d180f1c50ac09e67341320a3d82072f60f2a8ec1
dandnirv7 <dandnirv7@gmail.com>
2023-08-26 19:41:31 +0700

    refactor: update pinia setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import VueFeather from "vue-feather";
 
-createApp(App).component(VueFeather.name, VueFeather).use(router).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.component(VueFeather.name, VueFeather);
+app.use(router);
+app.mount("#app");
