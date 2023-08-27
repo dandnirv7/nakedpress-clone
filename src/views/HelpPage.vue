01f320d6414686c228b1c3f4f4884d1c8126310e
dandnirv7 <dandnirv7@gmail.com>
2023-08-27 12:55:06 +0700

    refactor: (+) Page (help)

---
+++ src/views/HelpPage.vue
@@ -0,0 +1,25 @@
+<script>
+import Whatsapp from "../components/Whatsapp.vue";
+import Accordion from "../components/Accordion.vue";
+import Download from "../components/Download.vue";
+export default {
+  components: {
+    Whatsapp,
+    Accordion,
+    Download,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Whatsapp />
+    <Accordion
+      :storeType="'faq'"
+      :title="'FAQ'"
+      :description="'Pertanyaan umum seputar nakedpress, detox, dan cold pressed juice'"
+      class="mt-10"
+    />
+    <Download />
+  </div>
+</template>
