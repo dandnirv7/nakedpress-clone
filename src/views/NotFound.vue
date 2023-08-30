2d43c195a08a7c39499ab8cdae13dedfca32138e
dandnirv7 <dandnirv7@gmail.com>
2023-08-30 11:20:22 +0700

    refactor: (+) 404 notfound pages

--- src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<script>
-export default {};
-</script>
-
-<template>
-  <div>
-    <h1>404</h1>
-  </div>
-</template>
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import NotFoundImage from "../assets/404-2021.svg";
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center px-5 space-y-5 my-10">
+    <h1 class="text-[64px] leading-10 font-bold">404</h1>
+    <img :src="NotFoundImage" alt="404 - Not Found" />
+    <p class="text-center">
+      Mohon maaf karena kami tidak dapat menemukan halaman yang Anda Harap
+      periksa kembali apakah alamat URL yang Anda ketik telah benar.
+    </p>
+    <router-link
+      to="/"
+      class="text-white bg-[#2f8d03] px-8 py-3 w-full text-center rounded-full font-semibold"
+    >
+      Kembali ke Halaman Beranda
+    </router-link>
+  </div>
+</template>
