2481a872713cf529cce205e0db5da35dd8545aff
dandnirv7 <dandnirv7@gmail.com>
2023-09-03 14:22:07 +0700

    refactor: (+) store pagination

---
+++ src/store/pagination.js
@@ -0,0 +1,24 @@
+import { defineStore } from "pinia";
+
+export const usePaginationStore = defineStore("pagination", {
+  state: () => ({
+    currentPage: 1,
+    postsPerPage: 10,
+    paginationItem: [],
+  }),
+  getters: {
+    totalPages: (state) =>
+      Math.ceil(state.paginationItem.length / state.postsPerPage),
+  },
+  actions: {
+    setCurrentPage(page) {
+      this.currentPage = page;
+    },
+
+    changePage(newPage) {
+      if (newPage >= 1 && newPage <= this.totalPages) {
+        this.currentPage = newPage;
+      }
+    },
+  },
+});
